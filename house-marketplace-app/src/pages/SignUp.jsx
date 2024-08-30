9497df01909ed7f7eaf82b2b79e23c0aab74882e
andrewteece <andrewteece@gmail.com>
2024-08-30 17:57:43 -0500

    test SignUp.jsx with firestore

--- house-marketplace-app/src/pages/SignUp.jsx
+++ house-marketplace-app/src/pages/SignUp.jsx
@@ -1,5 +1,12 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth'
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase.config'
 import ArrowRightIcon from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -27,17 +34,28 @@ function SignUp() {
     try {
       const auth = getAuth()
 
-      const userCredential = await signInWithEmailAndPassword(
+      const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
         password
       )
 
-      if (userCredential.user) {
-        navigate('/')
-      }
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      })
+
+      const formDataCopy = { ...formData }
+      delete formDataCopy.password
+      formDataCopy.timestamp = serverTimestamp()
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
+      navigate('/')
     } catch (error) {
-      toast.error('Bad User Credentials')
+      //toast.error('Something went wrong with registration')
+      console.log(error)
     }
   }
 
